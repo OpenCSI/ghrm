ec84b944011531c35fdcdb6ff4bd0cfc95801fdb
cjoron <cjoron@91db7de8-7983-4966-9c1d-ef8a34a1989e>
2011-09-22 12:22:02 +0000

    Update languages support + fix exception when cast variables
    git-svn-id: https://svn.asyd.net/svn/ghrm/trunk@56 91db7de8-7983-4966-9c1d-ef8a34a1989e

--- grails-app/controllers/com/opencsi/ghrm/controller/TaskInstanceController.groovy
+++ grails-app/controllers/com/opencsi/ghrm/controller/TaskInstanceController.groovy
@@ -12,20 +12,27 @@ class TaskInstanceController {
 
     def save = {
         def taskInstance = new TaskInstance()
-        taskInstance.project = Project.get(params.projectid)
-        taskInstance.user = User.get(params.userid)
-        taskInstance.task = Task.get(params.taskid)
-        taskInstance.days = (params.days).toFloat()
+        try{
+            taskInstance.project = Project.get(params.projectid)
+            taskInstance.user = User.get(params.userid)
+            taskInstance.task = Task.get(params.taskid)
+            taskInstance.days = (params.days).toFloat()
 
-        if(taskInstance.save(onFailError: true, flush:true)) {
-            // Send a notification mail:
-            def message = "Hello,\n the user '${taskInstance.user.firstname} ${taskInstance.user.lastname}' has a new task corresponding : '${taskInstance.task.name}' for the project : '${taskInstance.project.name}'.\n\nRegards.\n\n\nINFO: do not answer to this email."
-            MailService mail = new MailService()
-            flash.message = mail.sendMail("admin@opencsi.com","[GHRM] A new task for an user",message)//admin@opencsi.com
-            // redirect:
-            redirect(controller: 'project', action: 'show', id: params.projectid)
-        } else {
-            flash.message = "Error " + taskInstance.errors.allErrors.join(',')
+            if(taskInstance.save(onFailError: true, flush:true)) {
+                // Send a notification mail:
+                def message = "Hello,\n the user '${taskInstance.user.firstname} ${taskInstance.user.lastname}' has a new task corresponding : '${taskInstance.task.name}' for the project : '${taskInstance.project.name}'.\n\nRegards.\n\n\nINFO: do not answer to this email."
+                MailService mail = new MailService()
+                flash.message = mail.sendMail("admin@opencsi.com","[GHRM] A new task for an user",message)//admin@opencsi.com
+                // redirect:
+                redirect(controller: 'project', action: 'show', id: params.projectid)
+            } else {
+                flash.message = "Error " + taskInstance.errors.allErrors.join(',')
+                redirect(controller: 'project', action: 'show', id: params.projectid)
+            }
+        }
+        catch(Exception e)
+        {
+            flash.message = "${message(code:'task.error.submit')}"
             redirect(controller: 'project', action: 'show', id: params.projectid)
         }
     }
