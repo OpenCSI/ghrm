1b07869eb8be18f97377415c511b9d57d9c70930
cjoron <cjoron@91db7de8-7983-4966-9c1d-ef8a34a1989e>
2011-11-23 12:31:32 +0000

    Language improve
    git-svn-id: https://svn.asyd.net/svn/ghrm/trunk@128 91db7de8-7983-4966-9c1d-ef8a34a1989e

---
+++ grails-app/controllers/com/opencsi/ghrm/controller/ErrorController.groovy
@@ -0,0 +1,12 @@
+package com.opencsi.ghrm.controller
+
+class ErrorController {
+
+    def index = {
+        redirect(view:'error404')
+    }
+    
+    def error404 = {
+        flash.message = "${message(code:'error.404')}" + "<br><a href=\"javascript: history.go(-1)\">${message(code:'error.404.back')}</a>"
+    }
+}
