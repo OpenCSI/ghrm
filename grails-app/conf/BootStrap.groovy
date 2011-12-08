4bd02de109be1544eaa71fe675481eb6d58782f2
cjoron <cjoron@91db7de8-7983-4966-9c1d-ef8a34a1989e>
2011-12-08 08:37:21 +0000

    Fix update mod for dev issue in BootStrap.
    git-svn-id: https://svn.asyd.net/svn/ghrm/trunk@134 91db7de8-7983-4966-9c1d-ef8a34a1989e

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -84,41 +84,61 @@ class BootStrap {
                 new StatutRecruitment(name :"Accepted").save()
        // }
         if (Environment.current.name != 'production') {
-            new User(uid:"manager", firstname:"John", lastname:"Doe", email:"doe@opencsi.com").save(failOnError:true)
-            new User(uid:"brian", firstname: "Brian", lastname:"Jones", email:"brian@opencsi.com").save(failOnError:true)
-            new User(uid:"james", firstname: "James", lastname:"Smith", email:"james@opencsi.com").save(failOnError:true)
-            new User(uid:'cedric', firstname:'Cédric', lastname:'Joron', email:'un-autre-mail@opencsi.com').save(failOnError:true)
-            def ced = new ShiroUser(username:'cedric',passwordHash: new Sha256Hash("cedric").toHex())
-            ced.addToRoles(ShiroRole.findByName('employee'))
-            ced.addToRoles(ShiroRole.findByName('HR'))
-            ced.save(failOnError:true)
+            if (!User.findByUid('cedric'))
+			{
+				new User(uid:'cedric', firstname:'Cédric', lastname:'Joron', email:'un-autre-mail@opencsi.com').save(failOnError:true)
+				def ced = new ShiroUser(username:'cedric',passwordHash: new Sha256Hash("cedric").toHex())
+				ced.addToRoles(ShiroRole.findByName('employee'))
+				ced.addToRoles(ShiroRole.findByName('HR'))
+				ced.save(failOnError:true)
+			}
 
+			if (!User.findByUid('manager'))
+			{
+				new User(uid:"manager", firstname:"John", lastname:"Doe", email:"doe@opencsi.com").save(failOnError:true)
+				def manager = new ShiroUser(username: 'manager', passwordHash: new Sha256Hash("secret").toHex())
+				manager.addToRoles(ShiroRole.findByName('projectleader'))
+				manager.addToRoles(ShiroRole.findByName('employee'))
+				manager.save(failOnError:true)
+			}
 
-            def manager = new ShiroUser(username: 'manager', passwordHash: new Sha256Hash("secret").toHex())
-            manager.addToRoles(ShiroRole.findByName('projectleader'))
-            manager.addToRoles(ShiroRole.findByName('employee'))
-            manager.save(failOnError:true)
+			if (!User.findByUid('brian'))
+			{
+				new User(uid:"brian", firstname: "Brian", lastname:"Jones", email:"brian@opencsi.com").save(failOnError:true)
+				def brian = new ShiroUser(username: 'brian', passwordHash: new Sha256Hash("secret").toHex())
+				brian.addToRoles(ShiroRole.findByName('employee'))
+				brian.save(failOnError:true)
+			}
 
-            def brian = new ShiroUser(username: 'brian', passwordHash: new Sha256Hash("secret").toHex())
-            brian.addToRoles(ShiroRole.findByName('employee'))
-            brian.save(failOnError:true)
+			if (!User.findByUid('james'))
+			{
+				new User(uid:"james", firstname: "James", lastname:"Smith", email:"james@opencsi.com").save(failOnError:true)
+				def james = new ShiroUser(username: 'james', passwordHash: new Sha256Hash("secret").toHex())
+				james.addToRoles(ShiroRole.findByName('employee'))
+				james.save(failOnError:true)
+			}
+			if(!Customer.findByName("A big french company"))
+				new Customer(name:"A big french company", street:"12 rue de la soif", city:"Paris", postalCode: "75000").save(failOnError:true)
+            if(!Customer.findByName("A small french company"))
+				new Customer(name:"A small french company", street:"42 rue dtc", city:"Paris", postalCode: "75000").save(failOnError:true)
 
-            def james = new ShiroUser(username: 'james', passwordHash: new Sha256Hash("secret").toHex())
-            james.addToRoles(ShiroRole.findByName('employee'))
-            james.save(failOnError:true)
+			if(!Project.findByLabel("1000"))
+				new Project(name:"Project 1", customer: Customer.get(1),code: "C00101",description: "Assistance technique OpenAM",label: "1000",updateat: new Date(),createat: new Date()).save(failOnError:true)
+            if(!Project.findByLabel("1001"))
+				new Project(name:"Project 2", customer: Customer.get(2),code: "C00201",description: "Conseil securite",label: "1001",updateat: new Date(),createat: new Date()).save(failOnError:true)
 
-            new Customer(name:"A big french company", street:"12 rue de la soif", city:"Paris", postalCode: "75000").save(failOnError:true)
-            new Customer(name:"A small french company", street:"42 rue dtc", city:"Paris", postalCode: "75000").save(failOnError:true)
-
-            new Project(name:"Project 1", customer: Customer.get(1),code: "C00101",description: "Assistance technique OpenAM",label: "1000",updateat: new Date(),createat: new Date()).save(failOnError:true)
-            new Project(name:"Project 2", customer: Customer.get(2),code: "C00201",description: "Conseil securite",label: "1001",updateat: new Date(),createat: new Date()).save(failOnError:true)
-
-            new Task(name:"Assistance technique", label:"AT").save(failOnError:true)
-            new Task(name:"Project leader", label:"CP").save(failOnError:true)
-
-            new TaskInstance(project: Project.get(1), task: Task.findByLabel("AT"), user: User.get(3), days: 40).save(failOnError:true)
-            new TaskInstance(project: Project.get(1), task: Task.findByLabel("CP"), user: User.get(4), days: 4).save(failOnError:true)
-            new TaskInstance(project: Project.get(2), task: Task.findByLabel("AT"), user: User.get(4), days: 10).save(failOnError:true)
+			if(!Task.findByLabel("AT"))
+				new Task(name:"Assistance technique", label:"AT").save(failOnError:true)
+            if(!Task.findByLabel("CP"))
+				new Task(name:"Project leader", label:"CP").save(failOnError:true)
+			
+			if(!Task.findByLabel("AT"))
+			{
+				new TaskInstance(project: Project.get(1), task: Task.findByLabel("AT"), user: User.get(3), days: 40).save(failOnError:true)
+				new TaskInstance(project: Project.get(2), task: Task.findByLabel("AT"), user: User.get(4), days: 10).save(failOnError:true)
+			}
+			if(!Task.findByLabel("CP"))
+				new TaskInstance(project: Project.get(1), task: Task.findByLabel("CP"), user: User.get(4), days: 4).save(failOnError:true)
 
             def today = new DateTime()
 
